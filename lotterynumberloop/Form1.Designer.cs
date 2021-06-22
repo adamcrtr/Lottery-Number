5efa2d810d1d6babea2c0da21b1f18f7e67eee2b
adamcrtr <adampc123@hotmail.co.uk>
2021-06-22 16:27:50 +0100

    init commit

---
+++ lotterynumberloop/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace lotterynumberloop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLotto = new System.Windows.Forms.Button();
+            this.lstLottery = new System.Windows.Forms.ListBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLotto
+            // 
+            this.btnLotto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLotto.Location = new System.Drawing.Point(62, 39);
+            this.btnLotto.Name = "btnLotto";
+            this.btnLotto.Size = new System.Drawing.Size(168, 23);
+            this.btnLotto.TabIndex = 0;
+            this.btnLotto.Text = "Generate Numbers";
+            this.btnLotto.UseVisualStyleBackColor = true;
+            this.btnLotto.Click += new System.EventHandler(this.btnLotto_Click);
+            // 
+            // lstLottery
+            // 
+            this.lstLottery.FormattingEnabled = true;
+            this.lstLottery.Location = new System.Drawing.Point(62, 68);
+            this.lstLottery.Name = "lstLottery";
+            this.lstLottery.Size = new System.Drawing.Size(168, 95);
+            this.lstLottery.TabIndex = 1;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(62, 183);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(154, 183);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(316, 302);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lstLottery);
+            this.Controls.Add(this.btnLotto);
+            this.Name = "Form1";
+            this.Text = "Lottery Number Generator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLotto;
+        private System.Windows.Forms.ListBox lstLottery;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ lotterynumberloop/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lotterynumberloop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void btnLotto_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random(); //creates a random class
+            lstLottery.Items.Clear(); // clear current items in the list
+
+            int[] lottery_numbers;
+            lottery_numbers = new int[6]; //able to generate 6 numbers if the number between square brackets is changed example to 7 seven numbers will be generate
+
+           
+
+            for (int i = 0; i != (lottery_numbers.Length); i++)
+            {
+                lottery_numbers[i] = rnd.Next(1,49);
+                lstLottery.Items.Add(lottery_numbers[i]);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lstLottery.Items.Clear();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
